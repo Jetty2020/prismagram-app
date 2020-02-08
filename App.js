1954bc934fe4f8cb624a67ac715cfc0f2c0a1ebe
Jetty2020 <kmclo@naver.com>
2020-02-08 23:13:01 +0900

    #10.4 isLoggedIn part One

--- App.js
+++ App.js
@@ -7,12 +7,15 @@ import { Text, View, AsyncStorage } from "react-native";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { persistCache } from "apollo-cache-persist";
 import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
 import { ApolloProvider } from "react-apollo-hooks";
 import apolloClientOptions from "./apollo";
+import styles from "./styles";
 
  export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
@@ -28,6 +31,12 @@ import apolloClientOptions from "./apollo";
          cache,
          ...apolloClientOptions
        });
+       const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+       if (isLoggedIn === null || isLoggedIn === false) {
+         setIsLoggedIn(false);
+       } else {
+         setIsLoggedIn(true);
+       }
       setLoaded(true);
       setClient(client);
     } catch (e) {
@@ -37,11 +46,13 @@ import apolloClientOptions from "./apollo";
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded && client ? (
+  return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
-      <View>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ThemeProvider theme={styles}>
+         <View>
+           {isLoggedIn === true ? <Text>I'm in</Text> : <Text>I'm out</Text>}
+         </View>
+       </ThemeProvider>
     </ApolloProvider>
   ) : (
     <AppLoading />
