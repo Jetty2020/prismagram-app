18f7078f45020d8e4ad73cbcbb4bc6223586562c
Jetty2020 <kmclo@naver.com>
2020-02-09 09:32:59 +0900

    #11.1 AuthNavigation

---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,22 @@
+import { createAppContainer } from "react-navigation";
+ import { createStackNavigator } from "react-navigation-stack";
+
+ import Signup from "../screens/Auth/Signup";
+ import Confirm from "../screens/Auth/Confirm";
+ import Login from "../screens/Auth/Login";
+ import AuthHome from "../screens/Auth/AuthHome";
+
+ const AuthNavigation = createStackNavigator(
+   {
+     Signup,
+     Confirm,
+     Login,
+     AuthHome
+   },
+   {
+     initialRouteName: "AuthHome",
+     headerMode: "none"
+   }
+ );
+
+ export default createAppContainer(AuthNavigation);
