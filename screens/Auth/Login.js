090465ddc48e28f2730aa7eb118c939b2eb8f724
Jetty2020 <kmclo@naver.com>
2020-02-10 19:24:11 +0900

    #12.4~5 Login

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
  import styled from "styled-components";
  import { TouchableWithoutFeedback, Keyboard } from "react-native";
  import AuthButton from "../../components/AuthButton";
  import AuthInput from "../../components/AuthInput";
  import useInput from "../../hooks/useInput";
  import { Alert } from "react-native";
+ import { useMutation } from "react-apollo-hooks";
+ import { LOG_IN } from "./AuthQueries";
 
  const View = styled.View`
    justify-content: center;
@@ -12,9 +14,15 @@ import React from "react";
    flex: 1;
  `;
 
-export default () => {
+export default ({ navigation }) => {
   const emailInput = useInput("");
-  const handleLogin = () => {
+  const [loading, setLoading] = useState(false);
+   const [requestSecretMutation] = useMutation(LOG_IN, {
+     variables: {
+       email: emailInput.value
+     }
+   });
+   const handleLogin = async () => {
     const { value } = emailInput;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (value === "") {
@@ -23,6 +31,25 @@ export default () => {
       return Alert.alert("Please write an email");
     } else if (!emailRegex.test(value)) {
       return Alert.alert("That email is invalid");
+    }
+    try {
+      setLoading(true);
+      const {
+        data: { requestSecret }
+      } = await requestSecretMutation();
+      if (requestSecret) {
+        Alert.alert("Check your email");
+        navigation.navigate("Confirm");
+        return;
+      } else {
+        Alert.alert("Account not found");
+        navigation.navigate("Signup");
+      }
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Can't log in now");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -33,10 +60,10 @@ export default () => {
            placeholder="Email"
            keyboardType="email-address"
            returnKeyType="send"
-           onEndEditing={handleLogin}
+           onSubmitEditing={handleLogin}
            autoCorrect={false}
          />
-         <AuthButton onPress={handleLogin} text="Log In" />
+         <AuthButton loading={loading} onPress={handleLogin} text="Log In" />
        </View>
      </TouchableWithoutFeedback>
   );
