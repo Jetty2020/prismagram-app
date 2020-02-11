44544c36c45ddd5f6e3bb772002f13a12e4b34ba
Jetty2020 <kmclo@naver.com>
2020-02-11 17:33:28 +0900

    #12.9 Facebook Login part Two with error

--- screens/Auth/Signup.js
+++ screens/Auth/Signup.js
@@ -71,17 +71,24 @@ export default ({ navigation }) => {
   };
   const fbLogin = async () => {
     try {
+      setLoading(true);
       const { type, token } = await Facebook.logInWithReadPermissionsAsync(
         "769328456913303",
         {
-          permissions: ["public_profile"]
+          permissions: ["public_profile", "email"]
         }
       );
       if (type === "success") {
         const response = await fetch(
-          `https://graph.facebook.com/me?access_token=${token}`
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,last_name,first_name,email`
         );
-        Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+        const { email, first_name, last_name } = await response.json();
+         emailInput.setValue(email);
+         fNameInput.setValue(first_name);
+         lNameInput.setValue(last_name);
+         const [username] = email.split("@");
+         usernameInput.setValue(username);
+         setLoading(false);
       } else {
         // type === 'cancel'
       }
