8e69d4e6b7c384877c73b2cc48ce66748e7287ee
Jetty2020 <kmclo@naver.com>
2020-02-11 17:05:35 +0900

    #12.8 Facebook Login part One+error

--- components/AuthButton.js
+++ components/AuthButton.js
@@ -7,7 +7,8 @@ import React from "react";
  const Touchable = styled.TouchableOpacity``;
 
  const Container = styled.View`
-   background-color: ${props => props.theme.blueColor};
+   background-color: ${props =>
+     props.bgColor ? props.bgColor : props.theme.blueColor};
    padding: 10px;
    margin: 0px 50px;
    border-radius: 4px;
@@ -20,9 +21,9 @@ import React from "react";
    font-weight: 600;
  `;
 
-const AuthButton = ({ text, onPress, loading = false }) => (
+const AuthButton = ({ text, onPress, loading = false, bgColor = null }) => (
   <Touchable disabled={loading} onPress={onPress}>
-     <Container>
+     <Container bgColor={bgColor}>
      {loading ? <ActivityIndicator color={"white"} /> : <Text>{text}</Text>}
      </Container>
    </Touchable>
